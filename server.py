f418cd71d903194ea5c92faa652f38e603f379a0
WonPark <won.park19@gmail.com>
2014-10-18 13:46:43 -0700

    working on calculate

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from datetime import datetime
+from datetime import datetime, date
 import time
 
 
@@ -7,21 +7,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    render_template('index.html')
+    return render_template('index.html')
 
-def meal_point_calculator(mealpoints, date):
-	"""Returns a tuple"""
+def mealPointCalculator(points):
+    daysLeft = date.today()
+    semesterEnd = date(2014, 12, 19)
+    delta = (semesterEnd-daysLeft).days
+    return (round((points/delta),1), round((points*7/delta),1))
 
 
 """Render home page"""
 
 
 """Throws back a number"""
-@app.route("/<number>")
+@app.route("/results", methods = ["POST"])
 def calculate():
 	# dd/mm/yyyy format
-	print (time.strftime("%d/%m/%Y"))
+	##print (time.strftime("%d/%m/%Y"))
 	#mealpoint = meal_point_calculator(mealpoint,datetime.today())
+	return str(mealPointCalculator())
 
 if __name__ == "__main__":
     app.run(debug=True)
