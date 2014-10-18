b82b44a027c2c617e34807932d2ac134f545618b
WonPark <won.park19@gmail.com>
2014-10-18 14:07:47 -0700

    thanks bb allan

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime, date
 import time
 
@@ -15,17 +15,21 @@ def mealPointCalculator(points):
     delta = (semesterEnd-daysLeft).days
     return (round((points/delta),1), round((points*7/delta),1))
 
+def pointsToMeals(points):
+    dayMeals = round(mealPointCalculator(points)[0]/7.5, 1)
+    dayWeeks = round(mealPointCalculator(points)[1]/7.5, 1)
+    return(dayMeals,dayWeeks)
 
-"""Render home page"""
 
-
-"""Throws back a number"""
 @app.route("/results", methods = ["POST"])
 def calculate():
+	##print(point)
 	# dd/mm/yyyy format
 	##print (time.strftime("%d/%m/%Y"))
 	#mealpoint = meal_point_calculator(mealpoint,datetime.today())
-	return str(mealPointCalculator())
+	mealpoints = request.form['mealpoints']
+	return render_template('results.html', mealPoints = mealpoints)
+	##return str(mealPointCalculator(request.form['mealPoints']))
 
 if __name__ == "__main__":
     app.run(debug=True)
