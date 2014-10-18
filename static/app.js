61dd1de45da6b79bb99e12d2114101277d4bf720
Allan Zhou <allan.zhou@berkeley.edu>
2014-10-18 11:15:35 -0700

    Initial commit

---
+++ static/app.js
@@ -0,0 +1,8 @@
+(function(){
+  var app = angular.module('mealPoints', []);
+  //change the interpolation from {{ }} to [[ ]]
+  app.config(function($interpolateProvider){
+    $interpolateProvider.startSymbol('[[');
+    $interpolateProvider.endSymbol(']]');
+  });
+})();
