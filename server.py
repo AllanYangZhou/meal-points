140b4cd7a919e8b52e38ba4231af39a77d6e50cb
WonPark <won.park19@gmail.com>
2014-10-18 19:53:45 -0700

    moarrrr updates

--- server.py
+++ server.py
@@ -7,64 +7,181 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
 def mealPointCalculator(points):
-    daysLeft = date.today()
-    semesterEnd = date(2014, 12, 19)
-    delta = (semesterEnd-daysLeft).days
-    return (round((points/delta),1), round((points*7/delta),1))
+	daysLeft = date.today()
+	semesterEnd = date(2014, 12, 19)
+	delta = (semesterEnd-daysLeft).days
+	return (round((points/delta),1), round((points*7/delta),1))
 
 def pointsToMeals(points):
-    dayMeals = round(mealPointCalculator(points)[0]/7.5, 1)
-    dayWeeks = round(mealPointCalculator(points)[1]/7.5, 1)
-    return(dayMeals,dayWeeks)
+	dayMeals = round(mealPointCalculator(points)[0]/7.5, 1)
+	dayWeeks = round(mealPointCalculator(points)[1]/7.5, 1)
+	return(dayMeals,dayWeeks)
 
-def mealOptimize(points, lst):
-    cost = 0
-    for i in range(4, len(lst)):
-        if lst[i]== False:
-            if i % 3 == 1:
-                cost += 6
-            elif i % 3 == 2:
-                cost += 7
-            elif i % 3 == 0:
-                cost += 8
-    if lst[0] == False:
-        cost += 7
-    if lst[1]== False:
-        cost += 8
-    if lst[2]== False:
-        cost += 7
-    if lst[3]== False:
-        cost += 8
-    daysLeft = date.today()
-    semesterEnd = date(2014, 12, 19)
-    delta = (semesterEnd-daysLeft).days
-    weeks = delta//7
-    fullcost = weeks*cost
-    today = date.weekday(daysLeft)
-    if today != 5:
-        if today == 6:
-            if lst[2] == False:
-                fullcost += 7
-            if lst[3] == False:
-                fullcost += 8
-            today = 0
-        for i in range(today*3 + 4, len(lst)):
-            if lst[i] == False:
-                if i % 3 == 1:
-                    fullcost += 6
-                elif i % 3 == 2:
-                    fullcost += 7
-                elif i % 3 == 0:
-                    fullcost += 8
-                
+def mealCalculate(points, lst):
+	cost = 0
+	for i in range(4, len(lst)):
+		if lst[i]== False:
+			if i % 3 == 1:
+				cost += 6
+			elif i % 3 == 2:
+				cost += 7
+			elif i % 3 == 0:
+				cost += 8
+	if lst[0] == False:
+		cost += 7
+	if lst[1]== False:
+		cost += 8
+	if lst[2]== False:
+		cost += 7
+	if lst[3]== False:
+		cost += 8
+	daysLeft = date.today()
+	semesterEnd = date(2014, 12, 19)
+	delta = (semesterEnd-daysLeft).days
+	weeks = delta//7
+	fullcost = weeks*cost
+	today = date.weekday(daysLeft)
+	if today != 5:
+		if today == 6:
+			if lst[2] == False:
+				fullcost += 7
+			if lst[3] == False:
+				fullcost += 8
+			today = 0
+		for i in range(today*3 + 4, len(lst)):
+			if lst[i] == False:
+				if i % 3 == 1:
+					fullcost += 6
+				elif i % 3 == 2:
+					fullcost += 7
+				elif i % 3 == 0:
+					fullcost += 8
+				
 
-    if points >= fullcost:
-        return (points - fullcost, None, (points-fullcost)/weeks)
-    if points < fullcost:
-        return (points - fullcost, points//cost, (points-fullcost)/weeks)
+	if points >= fullcost:
+		return (points - fullcost, None, (points-fullcost)/weeks)
+	if points < fullcost:
+		return (points - fullcost, points//cost, (points-fullcost)/weeks) 
+
+""" def mealOptimize(points, lst, breakfast = True, lunch = True, dinner = True):
+	cost = 0
+	bcost = 6
+	lcost = 7
+	dcost = 8
+	if breakfast == False:
+		bcost = 3.5
+	if lunch == False:
+		lcost = 5
+	if dinner == False:
+		dcost = 6
+	for i in range(4, len(lst)):
+		if lst[i]:
+			if i % 3 == 1:
+				cost += bcost
+			elif i % 3 == 2:
+				cost += lcost
+			elif i % 3 == 0:
+				cost += dcost
+	if lst[0]:
+		cost += lcost
+	if lst[1]:
+		cost += dcost
+	if lst[2]:
+		cost += lcost
+	if lst[3]:
+		cost += dcost
+	daysLeft = date.today()
+	semesterEnd = date(2014, 12, 19)
+	delta = (semesterEnd-daysLeft).days
+	weeks = delta//7
+	fullcost = weeks*cost
+	today = date.weekday(daysLeft)
+	if today != 5:
+		if today == 6:
+			if lst[2]:
+				fullcost += lcost
+			if lst[3]:
+				fullcost += dcost
+			today = 0
+		for i in range(today*3 + 4, len(lst)):
+			if lst[i]:
+				if i % 3 == 1:
+					fullcost += bcost
+				elif i % 3 == 2:
+					fullcost += lcost
+				elif i % 3 == 0:
+					fullcost += dcost
+				
+	
+	if points >= fullcost:
+		return (points - fullcost, None, (points-fullcost)/weeks, (breakfast, lunch, dinner))
+	##if points + .15*points < fullcost:
+	  ##  return (points - fullcost, points//cost, 0, (breakfast, lunch, dinner))
+	elif breakfast == True:
+		return mealOptimize(points, lst, False)
+	elif lunch == True:
+		return mealOptimize(points,lst, False, False)
+	elif dinner == True:
+		return mealOptimize(points,lst, False, False, False)
+	return (points-fullcost, points//cost, 0, (False, False, False))"""
+def mealOptimize(pointsToSave, lst):
+	brunches, breakfasts, lunches, dinners = 0, 0, 0, 0
+	countBrunch, countBreakfast, countLunch, countDinner = 0,0,0,0
+	index = 4
+	if lst[0] == False:
+		brunches +=1
+
+	if lst[1] == False:
+		dinners +=1
+
+	if lst[2] == False:
+		brunches +=1
+
+	if lst[3] == False:
+		dinners+=1
+
+	while index<len(lst):
+		if lst[index] == False:
+			breakfasts+=1
+		index+=3
+
+	index = 5
+	while index<len(lst):
+		if lst[index] == False:
+			lunches +=1
+		index+=3
+
+	index = 6
+	while index<len(lst):
+		if lst[index] == False:
+			dinners+=1
+		index+=3
+
+	while abs(pointsToSave) >= 3 and (breakfasts > 0 or brunches > 0 or lunches > 0 or dinners> 0):
+		if breakfasts > 0:
+			breakfasts -= 1
+			countBreakfast  +=1
+			pointsToSave -= 6
+
+		elif brunches > 0:
+			 brunches -= 1
+			 countBrunch +=1
+			 pointsToSave -=7
+
+		elif lunches > 0:
+			lunches -= 1
+			countLunch +=1
+			pointsToSave -= 7
+
+		elif dinners >0:
+			dinners -= 1
+			countDinner +=1
+			pointsToSave -= 8
+
+	return [countBreakfast,countBrunch,countLunch,countDinner]
 
 @app.route("/results", methods = ["POST"])
 def calculate():
@@ -97,7 +214,7 @@ def calculate():
 	addlist("Friday-lunch")
 	addlist("Friday-dinner")
 
-	decision = mealOptimize(int(mealpoints), whichMeals)
+	decision = mealCalculate(int(mealpoints), whichMeals)
 
 
 	if decision[1] == None:
@@ -111,7 +228,16 @@ def calculate():
 		deficit = decision[0]
 		weeksleft = decision[1]
 		additionPoints = decision[2]
+
+		if abs(additionPoints) >= 10:
+			plan = mealOptimize(additionPoints, whichMeals)
+			breakfast = plan[0]
+			brunch = plan[1]
+			lunch  = plan[2]
+			dinner = plan[3]
+			return render_template('newplan.html', current = mealpoints, deficitPoints = abs(deficit), weeksleft = weeksleft, additionPoints = abs(additionPoints), breakfast = breakfast, brunch = brunch, lunch = lunch, dinner = dinner)
+
 		return render_template('deficit.html',current = mealpoints, deficitPoints = abs(deficit), weeksleft = weeksleft, additionPoints = abs(additionPoints) )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
