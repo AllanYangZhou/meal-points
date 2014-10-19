6acb48cb8dd090adb3ed4982ee345b7ed7fd9511
Allan Zhou <allan.zhou@berkeley.edu>
2014-10-19 00:00:17 -0700

    Trying to fix bug of missing weeks left

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from datetime import datetime, date
 import time
 
@@ -20,23 +20,23 @@ def pointsToMeals(points):
     dayWeeks = round(mealPointCalculator(points)[1]/7.5, 1)
     return(dayMeals,dayWeeks)
 
-def mealOptimize(points, lst):
+def mealCalculate(points, lst):
     cost = 0
     for i in range(4, len(lst)):
-        if lst[i]== False:
+        if lst[i]== True:
             if i % 3 == 1:
                 cost += 6
             elif i % 3 == 2:
                 cost += 7
             elif i % 3 == 0:
                 cost += 8
-    if lst[0] == False:
+    if lst[0] == True:
         cost += 7
-    if lst[1]== False:
+    if lst[1]== True:
         cost += 8
-    if lst[2]== False:
+    if lst[2]== True:
         cost += 7
-    if lst[3]== False:
+    if lst[3]== True:
         cost += 8
     daysLeft = date.today()
     semesterEnd = date(2014, 12, 19)
@@ -46,20 +46,21 @@ def mealOptimize(points, lst):
     today = date.weekday(daysLeft)
     if today != 5:
         if today == 6:
-            if lst[2] == False:
+            if lst[2] == True:
                 fullcost += 7
-            if lst[3] == False:
+            if lst[3] == True:
                 fullcost += 8
             today = 0
         for i in range(today*3 + 4, len(lst)):
-            if lst[i] == False:
+            if lst[i] == True:
                 if i % 3 == 1:
                     fullcost += 6
                 elif i % 3 == 2:
                     fullcost += 7
                 elif i % 3 == 0:
                     fullcost += 8
-
+                
+    print(fullcost)
     if points >= fullcost:
         return (points - fullcost, None, (points-fullcost)/weeks)
     if points < fullcost:
@@ -67,49 +68,56 @@ def mealOptimize(points, lst):
 
 @app.route("/results", methods = ["POST"])
 def calculate():
-    print(request.form)
-    mealpoints = request.form['mealpoints']
-    whichMeals = []    
+    json_data = request.get_json()
+    mealpoints = json_data['mealpoints']
+    whichMeals = []
     def addlist(meals):
-        if request.form.get(meals, True) == True:
+        if json_data[meals] == True:
+            print(meals)
             whichMeals.append(True)
         else:
             whichMeals.append(False)
 
-    addlist("Saturday-brunch")
-    addlist("Saturday-dinner")
-    addlist("Sunday-brunch")
-    addlist("Sunday-dinner")
-    addlist("Monday-breakfast")
-    addlist("Monday-lunch")
-    addlist("Monday-dinner")
-    addlist("Tuesday-breakfast")
-    addlist("Tuesday-lunch")
-    addlist("Tuesday-dinner")
-    addlist("Wednesday-breakfast")
-    addlist("Wednesday-lunch")
-    addlist("Thursday-breakfast")
-    addlist("Thursday-lunch")
-    addlist("Thursday-dinner")
-    addlist("Friday-breakfast")
-    addlist("Friday-lunch")
-    addlist("Friday-dinner")
-
-    decision = mealOptimize(int(mealpoints), whichMeals)
-
+    addlist("Saturdaybrunch")
+    addlist("Saturdaydinner")
+    addlist("Sundaybrunch")
+    addlist("Sundaydinner")
+    addlist("Mondaybreakfast")
+    addlist("Mondaylunch")
+    addlist("Mondaydinner")
+    addlist("Tuesdaybreakfast")
+    addlist("Tuesdaylunch")
+    addlist("Tuesdaydinner")
+    addlist("Wednesdaybreakfast")
+    addlist("Wednesdaylunch")
+    addlist("Wednesdaydinner")
+    addlist("Thursdaybreakfast")
+    addlist("Thursdaylunch")
+    addlist("Thursdaydinner")
+    addlist("Fridaybreakfast")
+    addlist("Fridaylunch")
+    addlist("Fridaydinner")
+    print(whichMeals[0])
+    decision = mealCalculate(float(mealpoints), whichMeals)
 
     if decision[1] == None:
         """Too many meal points"""
         excess = decision[0]
         mealPointsPerWeek = decision[2]
-        return render_template('results.html',current = mealpoints, excessPoints = excess, mealPointsPerWeek = mealPointsPerWeek)
+        #return render_template('results.html',current = mealpoints, excessPoints = excess, mealPointsPerWeek = mealPointsPerWeek)
+        #return [mealpoints, excess, mealPointsPerWeek]
+        f = {'result': 'true', 'mealpoints': mealpoints, 'excess': excess, 'mealPointsPerWeek': mealPointsPerWeek}
+        return jsonify(**f)
 
     else:
         """Too little meal points"""
         deficit = decision[0]
         weeksleft = decision[1]
         additionPoints = decision[2]
-        return render_template('deficit.html',current = mealpoints, deficitPoints = abs(deficit), weeksleft = weeksleft, additionPoints = abs(additionPoints) )
+        #return render_template('deficit.html',current = mealpoints, deficitPoints = abs(deficit), weeksleft = weeksleft, additionPoints = abs(additionPoints) )
+        #return [mealpoints, abs(deficit), weeksleft]
+        f = {'result': 'false', 'mealpoints': mealpoints, 'deficit': abs(deficit), 'weeksleft': weeksleft}
+        return jsonify(**f)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/app.js
+++ static/app.js
@@ -5,7 +5,15 @@
     $interpolateProvider.startSymbol('[[');
     $interpolateProvider.endSymbol(']]');
   });
-  app.controller('mealPlannerCtrl', function(){
+  app.controller('mealPlannerCtrl', ['$http', function($http){
+    var planner = this;
+    this.currentPts = 0;
+    this.excessPts = 0;
+    this.deficitPts = 0;
+    this.mealPointsPerWeek = 0;
+    this.weeksLeft = 'hello';
+    this.hasResults = false;
+    this.hasDeficit = false;
     this.weekEnds = [{name:'Saturday'}, {name:'Sunday'}];
     this.formData = {
       Mondaybreakfast : true,
@@ -28,5 +36,27 @@
       Sundaybrunch: true,
       Sundaydinner: true,
     };
-  });
+    this.sendData = function(){
+      $http.post('/results', this.formData)
+        .success(function(data, status, headers, config){
+          console.log(data.result)
+          if(data.result=='true'){
+            planner.hasResults = true;
+            planner.hasDeficit= false;
+            planner.currentPts = data.mealpoints;
+            planner.excessPts = data.excess;
+            planner.mealPointsPerWeek = data.mealPointsPerWeek;
+          } else {
+            planner.hasDeficit = true;
+            planner.hasResults = false;
+            planner.currentPts = data.mealpoints;
+            planner.deficitPts = data.deficit;
+            planner.weeksLeft = data.weeksLeft;
+          }
+        })
+        .error(function(data, status, headers, config){
+          console.log(data);
+        });
+    };
+  }]);
 })();
