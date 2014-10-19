2c5d7c5fc6796d239be04fc84c94e6b0fade37da
WonPark <won.park19@gmail.com>
2014-10-18 18:17:55 -0700

    Finally Works!

--- server.py
+++ server.py
@@ -23,20 +23,20 @@ def pointsToMeals(points):
 def mealOptimize(points, lst):
     cost = 0
     for i in range(4, len(lst)):
-        if lst[i]:
+        if lst[i]== False:
             if i % 3 == 1:
                 cost += 6
             elif i % 3 == 2:
                 cost += 7
             elif i % 3 == 0:
                 cost += 8
-    if lst[0]:
+    if lst[0] == False:
         cost += 7
-    if lst[1]:
+    if lst[1]== False:
         cost += 8
-    if lst[2]:
+    if lst[2]== False:
         cost += 7
-    if lst[3]:
+    if lst[3]== False:
         cost += 8
     daysLeft = date.today()
     semesterEnd = date(2014, 12, 19)
@@ -46,13 +46,13 @@ def mealOptimize(points, lst):
     today = date.weekday(daysLeft)
     if today != 5:
         if today == 6:
-            if lst[2]:
+            if lst[2] == False:
                 fullcost += 7
-            if lst[3]:
+            if lst[3] == False:
                 fullcost += 8
             today = 0
         for i in range(today*3 + 4, len(lst)):
-            if lst[i]:
+            if lst[i] == False:
                 if i % 3 == 1:
                     fullcost += 6
                 elif i % 3 == 2:
@@ -62,9 +62,9 @@ def mealOptimize(points, lst):
                 
     
     if points >= fullcost:
-        return (points - fullcost, None)
+        return (points - fullcost, None, (points-fullcost)/weeks)
     if points < fullcost:
-        return (points - fullcost, points//cost)
+        return (points - fullcost, points//cost, 0)
 
 @app.route("/results", methods = ["POST"])
 def calculate():
@@ -73,7 +73,8 @@ def calculate():
 	whichMeals = []
 
 	def addlist(meals):
-		if meals in request.form == True:
+		if request.form.get(meals, True) == True:
+			print("check")
 			whichMeals.append(True)
 		else:
 			whichMeals.append(False)
@@ -97,11 +98,22 @@ def calculate():
 	addlist("Friday-lunch")
 	addlist("Friday-dinner")
 
-	mealPointsPerDay = mealPointCalculator(int(mealpoints))[0]
-	mealPointsPerWeek = mealPointCalculator(int(mealpoints))[1]
-	meals = pointsToMeals(int(mealpoints))
-	return render_template('results.html',mealPoints = mealpoints ,mealPointsPerDay = mealPointsPerDay, mealPointsPerWeek = mealPointsPerWeek, meals = meals)
+	decision = mealOptimize(int(mealpoints), whichMeals)
+	print(int(mealpoints))
+	print(whichMeals)
+	print(decision)
 
+	if decision[1] == None:
+		"""Too many meal points"""
+		excess = decision[0]
+		mealPointsPerWeek = decision[2]
+		return render_template('results.html',current = mealpoints, excessPoints = excess, mealPointsPerWeek = mealPointsPerWeek)
+
+	else:
+		"""Too little meal points"""
+		deficit = decision[0]
+		weeksleft = decision[1]
+		return render_template('deficit.html',current = mealpoints, deficitPoints = abs(deficit), weeksleft = weeksleft)
 
 if __name__ == "__main__":
     app.run(debug=True)
